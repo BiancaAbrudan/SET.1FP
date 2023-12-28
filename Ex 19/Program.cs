aa19f34e680c234c6c8295b89f38be2400ddd03e
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:10:41 +0200

    Add files via upload

---
+++ Ex 19/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti un numar: ");
+            int numar = Convert.ToInt32(Console.ReadLine());
+
+            if (AreDoarDouaCifreRepetate(numar))
+            {
+                Console.WriteLine($"{numar} este format doar cu doua cifre care se pot repeta.");
+            }
+            else
+            {
+                Console.WriteLine($"{numar} nu este format doar cu doua cifre care se pot repeta.");
+            }
+        }
+
+        static bool AreDoarDouaCifreRepetate(int numar)
+        {
+            string numarString = numar.ToString();
+
+            return numarString.Length == 2 && numarString[0] == numarString[1];
+        }
+    }
+}
