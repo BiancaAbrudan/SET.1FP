009ec6cd48707ea9e290ddfcd07d0183234d0e5b
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:00:55 +0200

    Add files via upload

---
+++ Ex 8/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Swap_restrictionat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+
+            Console.WriteLine($"a={a} b={b}");
+
+        }
+    }
+}
