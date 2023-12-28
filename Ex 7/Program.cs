1b3d324d00e87b0c8615dc1a5745a566590ef153
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 14:59:57 +0200

    Add files via upload

---
+++ Ex 7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+
+            int aux=0;
+            
+            aux = a;
+            a = b;
+            b = aux;
+            Console.WriteLine($"a={a} b={b}");
+        }
+    }
+}
