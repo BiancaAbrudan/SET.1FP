bb74487deb8147bfea23cbff014a20f1ea2422c4
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 14:53:55 +0200

    Add files via upload

---
+++ Ex 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace NDivideK
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n, k;
+            Console.WriteLine("Introduceti n si k:");
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            if (k == 0)
+            {
+                Console.WriteLine("Nu se poate imparti la zero!");
+            }
+            else if (n % k == 0)
+            {
+                Console.WriteLine("n se divide cu k");
+            }
+            else
+            {
+                Console.WriteLine("n nu se divide cu k");
+            }
+        }
+    }
+}
+
