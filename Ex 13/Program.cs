c4b5937e7b60753b4b2fc6be074866b490dae9b5
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:05:58 +0200

    Add files via upload

---
+++ Ex 13/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Nr_an_bisecti_y1_y2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti primul an (y1): ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti ultimul an (y2): ");
+            int y2 = int.Parse(Console.ReadLine());
+            int NumarAniBisecti = CountLeapYears(y1, y2);
+            Console.Write($"Între anii {y1} și {y2} sunt {NumarAniBisecti} ani bisecți.");
+        }
+
+        private static int CountLeapYears(int y1, int y2)
+        {
+            int count = 0;
+            for (int y = y1; y <= y2; y++)
+            {
+                if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Ex 13/ProgramHelpers.cs
@@ -0,0 +1,16 @@
+﻿internal static class ProgramHelpers
+{
+
+    private static int CountLeapYears(int y1, int y2)
+    {
+        int count = 0;
+        for (int y = y1; y <= y2; y++)
+        {
+            if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
