1eef913f7b7de2081c891ef53a49f172abf6f7d8
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 14:58:46 +0200

    Add files via upload

---
+++ Ex 6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace verificatriunghi
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float a, b, c;
+            
+          
+            Console.Write(" Introduceti a = ");
+                 a = float.Parse(Console.ReadLine());
+
+            Console.Write(" Introduceti b = ");
+                 b = float.Parse(Console.ReadLine());
+
+            Console.Write(" Introduceti c = ");
+                 c = float.Parse(Console.ReadLine());
+
+            if(EsteTriunghi(a, b, c))
+            { 
+                Console.WriteLine(" Este triunghi."); 
+            }
+            else {
+                Console.WriteLine("Nu este triunghi.");
+            }
+
+            static bool EsteTriunghi(float a, float b, float c) 
+            {
+                return (a + b > c) && (b + c > a) && (a + c > b);
+            }
+
+        }
+    }
+}
