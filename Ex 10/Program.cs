0101b2b938e21bb9facce8d44d9ce2f13e95b290
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:03:17 +0200

    Add files via upload

---
+++ Ex 10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace test_primalitate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" n = ");
+            int n = int.Parse(Console.ReadLine());
+            if (EstePrim(n))
+            {
+                Console.Write($"{n} Este numar prim.");
+
+            }
+            else
+            {
+                Console.Write($"{n} Nu este numar prim.");
+            }
+        }
+            static bool EstePrim(int n)
+            {
+                if (n <= 1)
+                {
+                    return false;
+                }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+                    return true;
+                
+            }
+        
+    }
+}
