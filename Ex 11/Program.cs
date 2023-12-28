0a01a29e5e9fc317d5d295c9e0d63c347b06e1f1
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:04:06 +0200

    Add files via upload

---
+++ Ex 11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordine_inversa_n
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" n = ");
+            int n= int.Parse(Console.ReadLine());
+            Console.Write($"{n} Cifrele in ordine inversa sunt:");
+            PrintDigitsInReverseOrder(n);
+        }
+
+        private static void PrintDigitsInReverseOrder(int n)
+        {
+            string nString = n.ToString();
+            for(int i = nString.Length-1;i>=0;i--)
+            {
+                Console.Write(nString[i] + " ");
+            }
+          
+        }
+    }
+}
