2759946af571aab47981dd269c652240d4e78215
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:04:56 +0200

    Add files via upload

---
+++ Ex 12/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Nr_div_in_interval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Capatul inferior al intervalului (a):");
+            int a=int.Parse(Console.ReadLine());
+            Console.Write("Capatul superior al intervalului (b): ");
+            int b=int.Parse(Console.ReadLine());
+            int NumereDivizibile = CountDivisibleNumbers(n, a, b);
+            Console.WriteLine($"In intervalul [{a}, {b}] sunt {NumereDivizibile} numere divizibile cu {n}");
+        }
+
+        private static int CountDivisibleNumbers(int n, int a, int b)
+        {
+            int count = 0;
+            for(int i = a; i <= b; i++) 
+            {
+                if(i % n == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+    }
+}
