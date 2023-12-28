680db05d490ed4161b554b06fadfa2807ddd49d0
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 14:57:07 +0200

    Add files via upload

---
+++ Ex 5/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace A_k_a_cifra_a_nr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nr = ");
+            int nr = int.Parse(Console.ReadLine());
+
+            Console.Write("k = ");
+            int k = int.Parse(Console.ReadLine());
+
+            while(k > 1)
+            {
+                k--;
+                nr = nr / 10;
+            }
+
+            Console.WriteLine($"{nr%10}");
+
+
+        }
+    }
+}
