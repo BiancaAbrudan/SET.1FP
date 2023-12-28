d92d01cdcf285b3aad175ec92521fbc1d43162c6
BiancaAbrudan <146921015+BiancaAbrudan@users.noreply.github.com>
2023-12-28 15:02:22 +0200

    Add files via upload

---
+++ Ex 9/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Divizorii_lui_n
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+            }
+        }
+    }
+}
